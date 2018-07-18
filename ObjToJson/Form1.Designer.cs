6e5a2077458734dc9ce6e046fe69d3d5012c5458
Bruce Van Horn <brucevanhorn2@gmail.com>
2018-07-18 14:29:40 -0500

    initial commit

---
+++ ObjToJson/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ObjToJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnConvertToObject = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 26);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(776, 380);
+            this.txtCode.TabIndex = 0;
+            // 
+            // btnConvertToObject
+            // 
+            this.btnConvertToObject.Location = new System.Drawing.Point(499, 469);
+            this.btnConvertToObject.Name = "btnConvertToObject";
+            this.btnConvertToObject.Size = new System.Drawing.Size(247, 38);
+            this.btnConvertToObject.TabIndex = 2;
+            this.btnConvertToObject.Text = "JSON => Object";
+            this.btnConvertToObject.UseVisualStyleBackColor = true;
+            this.btnConvertToObject.Click += new System.EventHandler(this.btnConvertToObject_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.btnConvertToObject);
+            this.Controls.Add(this.txtCode);
+            this.Name = "Form1";
+            this.Text = "JSON to Object";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnConvertToObject;
+    }
+}
+
---
+++ ObjToJson/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjToJson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertToObject_Click(object sender, EventArgs e)
+        {
+            var code = txtCode.Text;
+            var sb = new StringBuilder();
+
+            using (StringReader reader = new StringReader(code))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    //split the string on : (assumes one property per line)
+                    var parts = line.Split(':');
+                    if (parts.Length == 2)
+                    {
+                        var prop = parts[0].Trim().Replace("\"", string.Empty);
+                        var newProp = prop + ": " + parts[1];
+                        sb.Append(newProp);
+                    } else
+                    {
+                        //just stick it back in (TWSS)
+                        sb.Append(line);
+                    }
+                }
+
+            }
+
+            txtCode.Text = sb.ToString();
+        }
+    }
+}
